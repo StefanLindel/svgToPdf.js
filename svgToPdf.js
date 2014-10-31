da3c77ae30b66167e189e676950abe19e1b065d6
Stefan Lindel <slin@cs.uni-kassel.de>
2014-10-31 11:39:42 +0100

    remove dependency of JQuery and fix some error with text item

--- svgToPdf.js
+++ svgToPdf.js
@@ -1,19 +1,19 @@
 /*
  * svgToPdf.js
- * 
+ *
  * Copyright 2012-2014 Florian Hülsmann <fh@cbix.de>
  * Copyright 2014 Ben Gribaudo <www.bengribaudo.com>
- * 
+ *
  * This script is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This script is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this file.  If not, see <http://www.gnu.org/licenses/>.
  * 
@@ -26,21 +26,9 @@ var pdfSvgAttr = {
     rect: ['x', 'y', 'width', 'height', 'stroke', 'fill', 'stroke-width'],
     ellipse: ['cx', 'cy', 'rx', 'ry', 'stroke', 'fill', 'stroke-width'],
     circle: ['cx', 'cy', 'r', 'stroke', 'fill', 'stroke-width'],
-    text: ['x', 'y', 'font-size', 'font-family', 'text-anchor', 'font-weight', 'font-style', 'fill']
+    text: ['x', 'y', 'font-size', 'font-family', 'text-anchor', 'font-weight', 'font-style', 'fill'],
+	path: []
 };
-
-var removeAttributes = function(node, attributes) {
-    var toRemove = [];
-    $.each(node.attributes, function(i, a) {
-        if(typeof(a) != 'undefined' && attributes.indexOf(a.name.toLowerCase()) == -1) {
-            toRemove.push(a.name);
-        }
-    });
-
-    $.each(toRemove, function(i, a) {
-        node.removeAttribute(a.name);
-    });	
-}
 
 var svgElementToPdf = function(element, pdf, options) {
     // pdf is a jsPDF object
@@ -48,14 +36,18 @@ var svgElementToPdf = function(element, pdf, options) {
     var remove = (typeof(options.removeInvalid) == 'undefined' ? false : options.removeInvalid);
     var k = (typeof(options.scale) == 'undefined' ? 1.0 : options.scale);
     var colorMode = null;
-    $(element).children().each(function(i, node) {
-        //console.log("passing: ", node);
-        var n = $(node);
+	if(typeof element ==="string") {
+		var el = document.createElement('div');
+		el.innerHTML = element;
+		element = el.childNodes[0];
+	}
+	for(var i=0;i<element.children.length;i++) {
+		var n = element.children[i];
 		var hasFillColor = false;
 		var hasStrokeColor = false;
-		if(n.is('g,line,rect,ellipse,circle,text')) {
-            var fillColor = n.attr('fill');
-            if(typeof(fillColor) != 'undefined') {
+		if('g,line,rect,ellipse,circle,text'.indexOf(n.tagName)>=0) {
+            var fillColor = n.getAttribute('fill');
+            if(fillColor) {
                 var fillRGB = new RGBColor(fillColor);
                 if(fillRGB.ok) {
 					hasFillColor = true;
@@ -65,15 +57,15 @@ var svgElementToPdf = function(element, pdf, options) {
                 }
             }
         }
-        if(n.is('g,line,rect,ellipse,circle')) {
+        if('g,line,rect,ellipse,circle'.indexOf(n.tagName)>=0) {
             if(hasFillColor) {
 				pdf.setFillColor(fillRGB.r, fillRGB.g, fillRGB.b);
 			}
-            if(typeof(n.attr('stroke-width')) != 'undefined') {
-                pdf.setLineWidth(k * parseInt(n.attr('stroke-width')));
+            if(n.getAttribute('stroke-width')) {
+                pdf.setLineWidth(k * parseInt(n.getAttribute('stroke-width')));
             }
-            var strokeColor = n.attr('stroke');
-            if(typeof(strokeColor) != 'undefined') {
+            var strokeColor = n.getAttribute('stroke');
+            if(strokeColor) {
                 var strokeRGB = new RGBColor(strokeColor);
                 if(strokeRGB.ok) {
 					hasStrokeColor = true;
@@ -88,113 +80,102 @@ var svgElementToPdf = function(element, pdf, options) {
                 }
             }
 		}
-        switch(n.get(0).tagName.toLowerCase()) {
+        switch(n.tagName.toLowerCase()) {
             case 'svg':
             case 'a':
             case 'g':
-                svgElementToPdf(node, pdf, options);
-                removeAttributes(node, pdfSvgAttr.g);
+                svgElementToPdf(n, pdf, options);
                 break;
             case 'line':
                 pdf.line(
-                    k*parseInt(n.attr('x1')),
-                    k*parseInt(n.attr('y1')),
-                    k*parseInt(n.attr('x2')),
-                    k*parseInt(n.attr('y2'))
+                    k*parseInt(n.getAttribute('x1')),
+                    k*parseInt(n.getAttribute('y1')),
+                    k*parseInt(n.getAttribute('x2')),
+                    k*parseInt(n.getAttribute('y2'))
                 );
-                removeAttributes(node, pdfSvgAttr.line);
                 break;
             case 'rect':
                 pdf.rect(
-                    k*parseInt(n.attr('x')),
-                    k*parseInt(n.attr('y')),
-                    k*parseInt(n.attr('width')),
-                    k*parseInt(n.attr('height')),
+                    k*parseInt(n.getAttribute('x')),
+                    k*parseInt(n.getAttribute('y')),
+                    k*parseInt(n.getAttribute('width')),
+                    k*parseInt(n.getAttribute('height')),
                     colorMode
                 );
-                removeAttributes(node, pdfSvgAttr.rect);
                 break;
             case 'ellipse':
                 pdf.ellipse(
-                    k*parseInt(n.attr('cx')),
-                    k*parseInt(n.attr('cy')),
-                    k*parseInt(n.attr('rx')),
-                    k*parseInt(n.attr('ry')),
+                    k*parseInt(n.getAttribute('cx')),
+                    k*parseInt(n.getAttribute('cy')),
+                    k*parseInt(n.getAttribute('rx')),
+                    k*parseInt(n.getAttribute('ry')),
                     colorMode
                 );
-                removeAttributes(node, pdfSvgAttr.ellipse);
                 break;
             case 'circle':
                 pdf.circle(
-                    k*parseInt(n.attr('cx')),
-                    k*parseInt(n.attr('cy')),
-                    k*parseInt(n.attr('r')),
+                    k*parseInt(n.getAttribute('cx')),
+                    k*parseInt(n.getAttribute('cy')),
+                    k*parseInt(n.getAttribute('r')),
                     colorMode
                 );
-                removeAttributes(node, pdfSvgAttr.circle);
                 break;
             case 'text':
-                if(node.hasAttribute('font-family')) {
-                    switch(n.attr('font-family').toLowerCase()) {
+                if(n.hasAttribute('font-family')) {
+                    switch(n.getAttribute('font-family').toLowerCase()) {
                         case 'serif': pdf.setFont('times'); break;
                         case 'monospace': pdf.setFont('courier'); break;
                         default:
-                            n.attr('font-family', 'sans-serif');
-                            pdf.setFont('helvetica');
+                            n.getAttribute('font-family', 'sans-serif');
+                            pdf.setFont('Helvetica');
                     }
                 }
                 if(hasFillColor) {
                     pdf.setTextColor(fillRGB.r, fillRGB.g, fillRGB.b);
                 }
                 var fontType = "";
-                if(node.hasAttribute('font-weight')) {
-                    if(n.attr('font-weight') == "bold") {
+                if(n.hasAttribute('font-weight')) {
+                    if(n.getAttribute('font-weight') == "bold") {
                         fontType = "bold";
-                    } else {
-                        node.removeAttribute('font-weight');
                     }
                 }
-                if(node.hasAttribute('font-style')) {
-                    if(n.attr('font-style') == "italic") {
+                if(n.hasAttribute('font-style')) {
+                    if(n.getAttribute('font-style') == "italic") {
                         fontType += "italic";
-                    } else {
-                        node.removeAttribute('font-style');
                     }
                 }
                 pdf.setFontType(fontType);
                 var pdfFontSize = 16;
-				if(node.hasAttribute('font-size')) {
-                    pdfFontSize = parseInt(n.attr('font-size'));
+				if(n.hasAttribute('font-size')) {
+                    pdfFontSize = parseInt(n.getAttribute('font-size'));
                 }
-                var box = node.getBBox();
+                var box = n.getBBox();
                 //FIXME: use more accurate positioning!!
-                var x, y, xOffset = 0;
-                if(node.hasAttribute('text-anchor')) {
-                    switch(n.attr('text-anchor')) {
+                var x=parseInt(n.getAttribute('x')), y=parseInt(n.getAttribute('y')), xOffset = 0;
+                if(n.hasAttribute('text-anchor')) {
+                    switch(n.getAttribute('text-anchor')) {
                         case 'end': xOffset = box.width; break;
                         case 'middle': xOffset = box.width / 2; break;
                         case 'start': break;
-                        case 'default': n.attr('text-anchor', 'start');
+                        case 'default': n.getAttribute('text-anchor', 'start');
                     }
-                    x = parseInt(n.attr('x')) - xOffset;
-                    y = parseInt(n.attr('y'));
+                    x = parseInt(n.getAttribute('x')) - xOffset;
                 }
 				//console.log("fontSize:", pdfFontSize, "text:", n.text());
                 pdf.setFontSize(pdfFontSize).text(
                     k * x,
                     k * y,
-                    n.text()
+                    n.innerHTML
                 );
-                removeAttributes(node, pdfSvgAttr.text);
                 break;
             //TODO: image
             default:
                 if(remove) {
-                    console.log("can't translate to pdf:", node);
-                    n.remove();
+                    console.log("can't translate to pdf:", n);
+                    element.removeChild(n);
                 }
         }
-    });
+    }
     return pdf;
 };
 
